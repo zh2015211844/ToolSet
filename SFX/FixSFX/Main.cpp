928859d1a372ed2eeefdb796d6f3f1a9c8a36275
zhangwei <zhangwei@qq.com>
2024-03-18 19:16:14 +0800

    自解压软件原理实现

---
+++ SFX/FixSFX/Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <windows.h>
+#include <WinBase.h>
+#include <cstdio>
+int main(int argc, const char* argv) {
+	
+	auto hUpdateRes = BeginUpdateResource(TEXT("SFX.exe"), FALSE);
+	if (hUpdateRes == NULL)
+	{
+		printf("加载文件失败！\n");
+		return 0;
+	}
+	char info[] = "\n-------------------------------\n\t\t\tsfx-fix\n-----------------------------------\n";
+
+	auto result = UpdateResource(hUpdateRes,    // update resource handle
+		TEXT("FILE"),                         // change dialog box resource
+		MAKEINTRESOURCE(102),         // dialog box id
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL),  // neutral language
+		info,                         // ptr to resource info
+		sizeof(info));       // size of resource info
+
+	if (result == FALSE)
+	{
+		printf("Could not add resource.");
+		return 0;
+	}
+
+	// Write changes to FOOT.EXE and then close it.
+	if (!EndUpdateResource(hUpdateRes, FALSE))
+	{
+		printf("Could not write changes to file.");
+		return 0;
+	}
+	return 0;
+}
---
+++ SFX/SFX/Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <windows.h>
+#include <WinBase.h>
+#include <cstdio>
+#include <atlstr.h>
+#include "resource.h"
+#include <clocale>
+int main(int argc, const char* argv)
+//int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nCmdShow)
+ {
+	//setlocale(LC_ALL, "zh-CN.UTF-8");
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	
+	auto name=MAKEINTRESOURCE(IDR_FILE1);
+	HRSRC res= FindResource(hInstance, name, TEXT("FILE"));
+	if (res == nullptr) {
+		printf("未找到资源！Error:%d\n", GetLastError());
+		return 0;
+	}
+	auto lres=LoadResource(hInstance, res);
+	if (lres == nullptr) {
+		printf("加载资源失败！");
+		return 0;
+	}
+	auto lpResLock = LockResource(lres);
+	if (lpResLock == NULL)
+	{
+		printf("获取资源地址失败！");
+		return 0;
+	}
+	char* str = (char*)lpResLock;
+	printf("\n字符串：%s\n", str);
+	return 0;
+}
